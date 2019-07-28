0ae2d66b8b4c46e640d821e5af450b306542286b
Rahul Sharma <rahulsharma15j@gmail.com>
2019-07-28 21:28:31 +0530

    redux added

---
+++ src/store/reducer.js
@@ -0,0 +1,10 @@
+import * as actionTypes from "./actions";
+
+const initialState = {
+  ingredients: null,
+  totalPrice: 4
+};
+
+const reducer = (state = initialState, actions) => {};
+
+export default reducer;
