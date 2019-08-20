a50fe52b75ff805472a610bf3be97d4a3225d33b
Rahul Sharma <46393429+rahulsharma15j@users.noreply.github.com>
2019-08-21 00:53:49 +0530

    Update auth.js

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -32,10 +32,10 @@ export const auth = (email, password, isSignup) => {
       returnSecureToken: true
     };
     let url =
-      "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAfAuHqtUd0KTVm9IWvtt0dIQFOJngLDMo";
+      "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=";
     if (!isSignup) {
       url =
-        "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyAfAuHqtUd0KTVm9IWvtt0dIQFOJngLDMo";
+        "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=";
     }
     axios
       .post(url, authData)
